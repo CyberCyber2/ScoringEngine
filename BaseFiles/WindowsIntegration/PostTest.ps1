d4d149cf89b909dca9a346b196bfa3274e022320
CyberCyber2 <56095531+CyberCyber2@users.noreply.github.com>
2021-02-16 16:14:38 -0500

    Update PostTest.ps1

--- BaseFiles/WindowsIntegration/PostTest.ps1
+++ BaseFiles/WindowsIntegration/PostTest.ps1
@@ -1,4 +1,4 @@
-$SERVER='http://192.168.15.132:443' 
+$SERVER='https://10.4.2.29' 
 $possible_users = @("joe", "bob", "tim")
 $data = $null
 $randSCR = $null
@@ -7,8 +7,8 @@ $key = 'cool'
 
 for ($i=1; $i -le 100; $i++) {
     $randUSR = Get-Random -Minimum 0 -Maximum 2
-    $randSCR = Get-Random -Minimum -1 -Maximum 4
-    $data = $possible_users[$randUSR].toString() + ":" + $($randSCR += Get-Random -Minimum -2 -Maximum 3) + ":" + $((New-TimeSpan -Start (Get-Date "01/01/1970") -End (Get-Date)).TotalSeconds) + ":" + $key.toString()
+    $randSCR = Get-Random -Minimum "-1" -Maximum 4
+    $data = $possible_users[$randUSR].toString() + ":" + $($randSCR += Get-Random -Minimum "-2" -Maximum 3) + ":" + $((New-TimeSpan -Start (Get-Date "01/01/1970") -End (Get-Date)).TotalSeconds) + ":" + $key.toString()
     Invoke-WebRequest -Uri $SERVER -Method POST -Body $data    
     sleep 5 
 }
