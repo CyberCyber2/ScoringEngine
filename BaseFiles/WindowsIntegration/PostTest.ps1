e340907f9e7764a85aa4c43f4935962199272338
CyberCyber2 <56095531+CyberCyber2@users.noreply.github.com>
2021-02-16 16:06:46 -0500

    Update and rename PostTest.py to PostTest.ps1

---
+++ BaseFiles/WindowsIntegration/PostTest.ps1
@@ -0,0 +1,14 @@
+$SERVER='http://192.168.15.132:443' 
+$possible_users = @("joe", "bob", "tim")
+$data = $null
+$randSCR = $null
+$randUSR = $null
+$key = 'cool'
+
+for ($i=1; $i -le 100; $i++) {
+    $randUSR = Get-Random -Minimum 0 -Maximum 2
+    $randSCR = Get-Random -Minimum -1 -Maximum 4
+    $data = $possible_users[$randUSR].toString() + ":" + $($randSCR += Get-Random -Minimum -2 -Maximum 3) + ":" + $((New-TimeSpan -Start (Get-Date "01/01/1970") -End (Get-Date)).TotalSeconds) + ":" + $key.toString()
+    Invoke-WebRequest -Uri $SERVER -Method POST -Body $data    
+    sleep 5 
+}
