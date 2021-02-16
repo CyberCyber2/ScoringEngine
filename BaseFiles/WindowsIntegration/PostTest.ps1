0540c61ef7b63ee77af73714dc131e42d142a70c
CyberCyber2 <56095531+CyberCyber2@users.noreply.github.com>
2021-02-16 17:06:05 -0500

    Update PostTest.ps1

--- BaseFiles/WindowsIntegration/PostTest.ps1
+++ BaseFiles/WindowsIntegration/PostTest.ps1
@@ -12,7 +12,7 @@ for ($i=1; $i -le 100; $i++) {
     $USR = $possible_users[$randUSR].toString()
     $SCR = $randSCR + (Get-Random -Minimum -"2" -Maximum 3)
     $TimeInSeconds = (New-Timespan -start "01/01/1970" -end (get-date)).totalseconds.tostring().split(".")[0]
-    $TimeInMinutes = $TimeInSeconds / 60
+    $TimeInMinutes = [math]::Floor($TimeInSeconds / 60)
     $Body = $data -f $USR,$SCR,$TimeInSeconds,$key
     Invoke-WebRequest -SkipCertificateCheck -Uri $SERVER -Method POST -Body $Body   
     sleep 5 
