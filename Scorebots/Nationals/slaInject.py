2b014da1e47242047d6e11ecc6964f83d2394536
CyberCyber2 <56095531+CyberCyber2@users.noreply.github.com>
2021-03-08 15:12:44 -0500

    Update slaInject.py

--- Scorebots/Nationals/slaInject.py
+++ Scorebots/Nationals/slaInject.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-#runs on the SERVER
 import matplotlib.pyplot as plt 
 import subprocess, platform, os
 import hashlib
@@ -7,8 +5,8 @@ import urllib.request as urllib2
 import re
 import time
 from difflib import SequenceMatcher
-#
 ###############CONFIG###################
+checkInterval = 5
 ftpFile = "FTPCHECK.txt"
 ftpFileHash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
 ftpPath = "/home/joe/" #Path on the client where the file to be checked is stored
@@ -16,20 +14,19 @@ apacheCheck = "Check2132" #string to look for in apache2 page
 apacheWebsite = "index.html" #page to look for for string
 checkInterval = 5
 dataDirectory = "/home/server/Desktop/data/" #directory on server to store data
-########################################
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 class Team:
     def __init__(self, name, address):
         self.name = name
         self.address = address
-        self.internetHIST = []
         self.ftpHIST = []
         self.apacheHIST = []
     def getName(self):
         return self.name
     def getAddress(self):
-        return self.address    
+        return self.address
     def countUptime(self, service): 
-        self.internetFileData = "/home/server/Desktop/data" + self.name + "INTERNETHIST.dat" #storage location for previous history. Ex: Computer1INTERNETHIST.dat
+
         if service == "internet":
             if (checkInternet(self.address) == "Ok"):
                 self.internetHIST.append("1")
@@ -40,11 +37,8 @@ class Team:
                 print("Internet unreliable, added 0")
                 print(self.internetHIST)
             internetReliability = str(self.internetHIST.count("1") + self.internetHIST.count("0") * (-5))
-            writeToFile(self.internetFileData, self.internetHIST)
             return internetReliability
- 
-            #~~~~~~~~~~~~~~~~#
- 
+
         if service == "ftp":
             if (checkFTP(str(self.getAddress()),"21",ftpFile, ftpFileHash) == "Ok"):
                 self.ftpHIST.append("1")
@@ -56,7 +50,7 @@ class Team:
                 print(self.ftpHIST)
             ftpReliability = str(self.ftpHIST.count("1") + self.ftpHIST.count("0") * (-5))
             return ftpReliability
- 
+
         if service == "apache":
             if (check_apache2(str(self.getAddress())) == "Ok"):
                 self.apacheHIST.append("1")
@@ -68,38 +62,27 @@ class Team:
                 print(self.apacheHIST)
             apacheReliability = str(self.apacheHIST.count("1") + self.apacheHIST.count("0") * (-5))
             return apacheReliability
-########################################################################
+
 allTeams = [
-    Team('Erdie', '127.0.0.1'),
-    Team('Matt', '127.0.0.1')
+    Team('Computer1', '127.0.0.1'),
+    Team('Computer2', '192.168.1.5')
 ]
-########################################################################
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 def similar(a, b): #Just incase the hash has an extra space, don't feel like removing space
     return SequenceMatcher(None, a, b).ratio()
- 
-def readData(filename):
-    with open(filename) as f:
-        lines = [line.rstrip() for line in f]
- 
-def writeToFile(filename, dataList):
-    with open('filename', 'w') as f:
-        for item in dataList:
-            f.write("%s\n" % item)
-#~~~~~~~~~~~~~~PING~~~~~~~~~~~~~~~#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 def checkInternet(ip):
     hostname = ip
     response = os.system("ping -c 1 " + hostname)
+    pingstatus = "Fail"
     if response == 0:
         pingstatus = "Ok"
     else:
         pingstatus = "Fail"
     return pingstatus
-#~~~~~~~~~~~~~~FTP~~~~~~~~~~~~~~~#
-import ftplib
-#You must place a checkfile in the ftp directory on the CLIENT
-#checkfile: file to look for on CLIENT
-#filehash: correct hash to check for
+
 def checkFTP(ip, port, checkfile, filehash):
+    ftpstatus = "Fail"
     try:
         ftp = ftplib.FTP(ip) 
         ftp.login("joe", "joe")
@@ -109,140 +92,71 @@ def checkFTP(ip, port, checkfile, filehash):
         checkHash = os.popen(("sha1sum " + f + "| cut -d' ' -f1")).read()
         if (float(similar(checkHash,filehash)) >= 0.9):
             print("FTP hashes match")
-            return ("Ok")
+            ftpstatus = "Ok"
         else:
             print("Client Hash: " + checkHash + " Server Hash: " + ftpFileHash)
-            return ("Fail")
+            ftpstatus = "Fail"
         ftp.quit()
     except Exception as e:
         print("FTP hash exception: " + str(e) + " for " + str(ip))
-        return ("Fail")
-#~~~~~~~~~~~~~~SMTP~~~~~~~~~~~~~~~#
-from smtplib import SMTP
-def check_smtp(ip, port):
-    try:
-        smtp = SMTP(ip, port)
-        smtp.quit()
-        return ("Ok")
-    except Exception as e:
-        return ("Fail")
-#~~~~~~~~~~~~~~APACHE2~~~~~~~~~~~~~~~#
-#check to see if a certain text appears on the webserver
+        ftpstatus = "Fail"
+    return (ftpstatus)
+
 def check_apache2(ip):
+    apachestatus = "Fail"
     try:
         html_content = urllib2.urlopen("http://" + ip + "/" + apacheWebsite).read().decode('utf-8')
         matches = re.findall(str(apacheCheck), html_content);
         if len(matches) == 0: 
             print ('apache2 text not found')
-            return ("Fail")
+            apachestatus = "Fail"
         else:
             print ('apache2 text found')
-            return ("Ok")
+            apachestatus = "Ok"
     except Exception as e:
         print("apache2 exception: " + str(e))
-        return ("Fail")
-#~~~~~~~~~~~~~~DNS~~~~~~~~~~~~~~~#
-def check_dns(ip, port, query, query_type, answer):
-    res = resolver.Resolver()
-    res.nameservers = [ip]
-    try:
-        query_answer = str(res.query(query, query_type).rrset[0])
-        print("[DEBUG-DNS] DNS answered", query_answer)
-        if answer != query_answer:
-            return (0, "DNS server returned incorrect answer " +\
-                        query_answer + " to query " + query +". Correct \
-                        answer was " + answer + ".")
-        return ("Ok")
-    except Exception as e:
-        return ("Fail")
-#~~~~~~~~~~~~~~RDP~~~~~~~~~~~~~~~#
-#~~~~~~~~~~~~~~SMB~~~~~~~~~~~~~~~#
-def check_smb(ip, port, anon, share, checkfile, filehash, domain):
-    path = '//{}/{}'.format(ip, share)
-    if checkfile and filehash:
-        tmpfile = path + "tmpfiles/smb-%s-%s-%s-checkfile" % (share, ip, checkfile)
-        cmd = 'get "{}" "{}"'.format(checkfile, tmpfile)
-        smbcli = ['smbclient', "-N", path, '-c', cmd]
-        if domain:
-            smbcli.extend(['-W', domain])
-        #try:
-        print("[DEBUG-SMB] Grabbing file for", ip)
-        output = subprocess.check_output(smbcli, stderr=subprocess.STDOUT)
-        with open(tmpfile, "rb") as f:
-            content = f.read()
-            checkhash = hashlib.sha1(content).hexdigest();
-        if checkhash != filehash:
-            return(0, "Hash %s does not match %s." % (checkhash, filehash))
-        return 1, None
-        #except Exception as e:
-        #    return 0, str(e)
-    else:
-        print("[DEBUG-SMB] Trying anonymous/noauth for", ip)
-        smbcli = ['smbclient', "-N", "-L",  path]
-        if domain:
-            smbcli.extend(['-W', domain])
-        try:
-            output = subprocess.check_output(smbcli, stderr=subprocess.STDOUT)
-            return ("Ok")
-        except Exception as e:
-            return ("Fail")
-#~~~~~~~~~~~~~~SSH~~~~~~~~~~~~~~~#
-#from paramiko import client, RSAKey
-#from paramiko.ssh_exception import *
-#import socket
-def check_ssh(ip, port, user, private_key):
-    try:
-        cli = client.SSHClient()
-        cli.load_host_keys("/dev/null")
-        cli.set_missing_host_key_policy(client.AutoAddPolicy())
-        if private_key and user:
-            print("[DEBUG-SSH] Trying pubkey auth for", ip)
-            k = RSAKey.from_private_key_file(path + "checkfiles/" + private_key)
-            cli.connect(ip, port, user, banner_timeout=20, timeout=20, auth_timeout=20, pkey=k)
-        else:
-            cli.connect(ip, port, "root", "Password3#", banner_timeout=20, timeout=20, auth_timeout=20)
-        cli.close()
-        return ("Ok")
-    except Exception as e:
-        if str(e) == "Authentication failed." and not private_key:
-            return ("Ok")
-        return ("Fail")
-########################################################################
-def main():
-    while (True): 
+    
+    return (apachestatus)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+def grapherFunction():
+   while (True): 
         fig = plt.figure(dpi=80)
         ax = fig.add_subplot(1,1,1)
-        table_data=[[" "], ["Internet"], ["FTP"] ]
+        table_data=[[" "], ["Internet"], ["FTP"], ["Apache2"] ]
         for t in allTeams:
             table_data[0].append(t.getName())
             table_data[1].append(str(checkInternet (str(t.getAddress()) )) + ":" + str(t.countUptime("internet")))
             table_data[2].append(str(checkFTP(str(t.getAddress()),"21", ftpFile, ftpFileHash)) + ":" + str(t.countUptime("ftp")))
-            #table_data[1].append(str(check_apache2(str(t.getAddress()))) + ":" + str(t.countUptime("apache")))
+            table_data[3].append(str(check_apache2(str(t.getAddress()))) + ":" + str(t.countUptime("apache")))
         table = ax.table(cellText=table_data, loc='center')
         table.set_fontsize(14)
         table.scale(1,4)
         ax.axis('off')
-        plt.savefig("InjectSLA.png")
+        plt.savefig("graphReliability.png")
         time.sleep(checkInterval)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+def main():
+    grapherFunction()
+    #~~~~~~~~~~~~~~~~~~#
+    import multiprocessing
+    processes = [ 
+    multiprocessing.Process(target=checkInternet()), 
+    multiprocessing.Process(target=checkFileFTP()),
+    multiprocessing.Process(target=check_apache2())
+
+    ]
  
+    if process.is_alive():
+        p.terminate()
+
+        for p in processes: 
+            p.start()
+        for q in processes: 
+            q.join(5)
+
+
 if __name__ == "__main__":
     main()
- 
-################################
-import multiprocessing
-
-
-def checkInternet(): pass
-
-process = multiprocessing.Process(target=checkInternet())
-process.start() # start the function
-process.join(10) # wait max 10 seconds for functionA to finish
-
-if process.is_alive():
-  # The function is still running
-  p.terminate()
-
-for p in processes:
-  p.start()
-for p in processes:
-  p.join(10)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
