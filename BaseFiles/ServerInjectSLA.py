fdd3f8c22b00f11a745d2b33285a0714e9d73df4
CyberCyber2 <56095531+CyberCyber2@users.noreply.github.com>
2021-03-10 14:27:40 -0500

    Update ServerInjectSLA.py

--- BaseFiles/ServerInjectSLA.py
+++ BaseFiles/ServerInjectSLA.py
@@ -5,15 +5,14 @@ import urllib.request
 import re
 import time
 from difflib import SequenceMatcher
-###############CONFIG###################
-checkInterval = 5
-ftpFile = "FTPCHECK.txt"
-ftpFileHash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
-ftpPath = "/home/joe/" #Path on the client where the file to be checked is stored
-apacheCheck = "Check2132" #string to look for in apache2 page
-apacheWebsite = "index.html" #page to look for for string
-checkInterval = 5
-dataDirectory = "/home/server/Desktop/data/" #directory on server to store data
+import configparser
+from configparser import *
+import os.path
+#import mysql.connector
+from urllib.error import HTTPError, URLError
+import logging
+import socket
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 class Team:
     def __init__(self, name, address):
@@ -22,6 +21,7 @@ class Team:
         self.ftpHIST = []
         self.internetHIST = []
         self.apacheHIST = []
+        self.sambaHIST = []
     def getName(self):
         return self.name
     def getAddress(self):
@@ -67,13 +67,31 @@ class Team:
             apacheReliability = str(self.apacheHIST.count("1") + self.apacheHIST.count("0") * (-5))
             return apacheReliability
 
-allTeams = [
-    Team('Computer1', '127.0.0.1'),
-    Team('Computer2', '10.4.2.19')
-]
+        if service == "samba":
+            print("Service is samba")
+            if (checkSMB(str(self.getAddress()) , str(self.getName())) == "Ok"):
+                self.sambaHIST.append("1")
+                print("samba reliable, added 1")
+                print(self.sambaHIST)
+            else:
+                self.sambaHIST.append("0")
+                print("samba unreliable, added 0")
+                print(self.sambaHIST)
+            sambaReliability = str(self.sambaHIST.count("1") + self.sambaHIST.count("0") * (-5))
+            return sambaReliability    
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+class CaseConfigParser(SafeConfigParser):
+     def optionxform(self, optionstr):
+         return optionstr
+allTeams = [
+    Team('Computer1', '192.168.1.240'),
+    Team('Computer2', '127.0.0.1')
+]
 def similar(a, b): #Just incase the hash has an extra space, don't feel like removing space
     return SequenceMatcher(None, a, b).ratio()
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 def checkInternet(ip):
     hostname = ip
@@ -105,11 +123,42 @@ def checkFTP(ip, port, checkfile, filehash):
         print("FTP hash exception: " + str(e) + " for " + str(ip))
         ftpstatus = "Fail"
     return (ftpstatus)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+def checkSMB(ip, teamName):
+    tempFileCheck = str(teamName) + ".txt" #tell them to add this file on client
+    try:
+        subprocess.call(['smbget', "smb://" + str(ip) + "/" + str(smbSHR) + "/" + str(tempFileCheck), "-n"])
+        if not (os.path.isfile(str(tempFileCheck))):
+            print("Error: could not get SMB file: " + str(tempFileCheck) + " from " + str(ip))
+            return ("Fail")
+        subprocess.call(['rm', '-rf', str(tempFileCheck)])
+        return("Ok")
+        print ("Deleted file: " +  str(tempFileCheck))
+    except Exception as e:
+        print("SMB ERROR: " + str(e))
+        subprocess.call(['rm', '-rf', str(tempFileCheck)])
+        return ("Fail")
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+def checkDNS(ip, recordType): pass
+#dig google.com ANY +noall +answer
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-from urllib.error import HTTPError, URLError
-import logging
-import socket
+def checkRDP(ip, port): pass
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+def checkSSH(ip, port): pass
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+def checkVNC(ip, port): pass
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+def checkSMTP(ip, port): pass
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+def checkMYSQL(ip, port): pass
+ #   mydb = mysql.connector.connect(host=str(ip), user=mySQL_USR, password=mySQL_PWD, database=mySQL_DB )
+ #   mycursor = mydb.cursor()
+ #   mycursor.execute(mySQL_Query) #SELECT name, address FROM customers
+ #   myresult = mycursor.fetchall()
+ #   for x in myresult:
+        #check
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 def check_apache2(ip):
     url = str ("http://" + ip + "/" + apacheWebsite)
     try:
@@ -133,18 +182,37 @@ def check_apache2(ip):
             print ('apache2 text found')
             return("Ok")
     return("Fail")
-
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 def grapherFunction():
-   while (True): 
+    while (True): 
+    #~~~~~~~~Variables~~~~~~~~#
+        #Below Parameters on Injects.cnf. Must be same for ALL clients
+        config = CaseConfigParser(os.environ)
+        config.read('Injects.cnf')
+        checkInterval = int(config.get('General', 'checkInterval'))
+        ftpFile = config.get('General', 'ftpFile')
+        ftpFileHash = config.get('General', 'ftpFileHash')
+        ftpPath = config.get('General', 'ftpPath') #Path on the client where the file to be checked is stored
+        apacheCheck = config.get('General', 'apacheCheck') #string to look for in apache2 page
+        apacheWebsite = config.get('General', 'apacheWebsite') #page to look for for string
+        dataDirectory = config.get('General', 'dataDirectory') #directory on server to store data
+        mySQL_Query = config.get('General', 'mySQL_Query')
+        smbUSR = config.get('General', 'smbUSR')
+        smbPWD = config.get('General', 'smbPWD')
+        smbSHR = config.get('General', 'smbSHR')
+    #~~~~~~~~~PLOT~~~~~~~~~# 
         fig = plt.figure(dpi=80)
         ax = fig.add_subplot(1,1,1)
-        table_data=[[" "], ["Apache2"] ]
+        table_data=[[" "], ["Samba"] ]
         for t in allTeams:
             table_data[0].append(t.getName())
-           # table_data[1].append(str(checkInternet (str(t.getAddress()) )) + ":" + str(t.countUptime("internet")))
+            #table_data[1].append(str(checkInternet (str(t.getAddress()) )) + ":" + str(t.countUptime("internet")))
             #table_data[2].append(str(checkFTP(str(t.getAddress()),"21", ftpFile, ftpFileHash)) + ":" + str(t.countUptime("ftp")))
-            table_data[1].append(str(t.getApache()) + ":" + str(t.countUptime("apache")))
+            #table_data[1].append(str(t.getApache()) + ":" + str(t.countUptime("apache")))
+
+            #SMB VARIABLE PROBLEM
+            table_data[1].append(str( checkSMB(t.getAddress(), t.getName())   ) + ":" + str(t.countUptime("samba")))
         table = ax.table(cellText=table_data, loc='center')
         table.set_fontsize(14)
         table.scale(1,4)
@@ -153,10 +221,10 @@ def grapherFunction():
         time.sleep(checkInterval)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 def main():
     grapherFunction()
    
 if __name__ == "__main__":
     main()
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
